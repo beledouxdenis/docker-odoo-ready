673432cf9774349b922337405a02647afb6f19ae
Denis Ledoux <dle@odoo.com>
2026-07-03 16:02:13 +0200

    [IMP] containers/podman-broker: allow codex to inspect and exec
    the containers it spawned

--- containers/podman-broker/broker.py
+++ containers/podman-broker/broker.py
@@ -2,6 +2,7 @@
 import argparse
 import os
 import pty
+import re
 import select
 import signal
 import socketserver
@@ -15,11 +16,48 @@ PARSER = argparse.ArgumentParser(add_help=False)
 PARSER.add_argument("--volume", "-v", action="append")
 PARSER.add_argument("--image", action="append")
 PARSER.add_argument("--build", action="store_true")
+CONTAINER_NAME_RE = re.compile(r"odoo-[0-9a-f]{7}")
+EXEC_PARSER = argparse.ArgumentParser(add_help=False)
+EXEC_PARSER.add_argument("--detach", action="store_true")
+EXEC_PARSER.add_argument("--env", action="append")
+EXEC_PARSER.add_argument("--user")
+EXEC_PARSER.add_argument("--workdir")
+
+
+def is_codex_odoo_container(name):
+    return bool(CONTAINER_NAME_RE.fullmatch(name))
+
+
+def is_allowed_exec(argv):
+    if len(argv) < 3 or not is_codex_odoo_container(argv[1]):
+        return False
+    try:
+        _, command = EXEC_PARSER.parse_known_args(argv[2:])
+    except SystemExit:
+        return False
+    return bool(command) and not command[0].startswith("-")
+
+
+def is_allowed_inspect(argv):
+    return len(argv) == 2 and is_codex_odoo_container(argv[1])
+
+
+PODMAN_COMMANDS = {
+    "exec": is_allowed_exec,
+    "inspect": is_allowed_inspect,
+}
 
 
 class Handler(socketserver.BaseRequestHandler):
     def handle(self):
         argv = self.request.makefile("rb").readline().rstrip(b"\n").decode().split("\0")
+        if argv and argv[0] in PODMAN_COMMANDS:
+            if PODMAN_COMMANDS[argv[0]](argv):
+                process = subprocess.run(["podman", *argv], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                self.request.sendall(process.stdout)
+            else:
+                self.request.sendall(f"denied docker-odoo {' '.join(argv)}\n".encode())
+            return
         if any(vars(PARSER.parse_known_args(argv)[0]).values()):
             self.request.sendall(f"denied docker-odoo {' '.join(argv)}\n".encode())
             return
